4192f6cf8bfca2e05df9660377a3315b596986f3
Fortune Mahloane <sensefortune01@gmail.com>
2025-10-24 11:36:46 +0200

    connect to db, install jwt

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import connectDB from './config/db'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+connectDB()
 
 app.use(cors({origin: '*',credentials: true}))
 app.use(express.json())
