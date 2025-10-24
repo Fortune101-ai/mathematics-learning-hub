e8c3f05d39b65d964721749350e2744ed3794f7e
Fortune Mahloane <sensefortune01@gmail.com>
2025-10-24 11:19:04 +0200

    install winston logger, setup logging and mongodb configuration

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import logger from "./logger";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        logger.info('MongoDB Connected')
+
+    } catch (error) {
+        logger.error('MongoDB Connection Failed', error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
