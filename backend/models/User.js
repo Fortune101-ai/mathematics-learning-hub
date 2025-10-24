55ce4d70939a220b82682094775f67301cb2688d
Fortune Mahloane <sensefortune01@gmail.com>
2025-10-24 11:27:51 +0200

    install mongoose, bcrypt and create a user model

---
+++ backend/models/User.js
@@ -0,0 +1,75 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
+
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["learner", "tutor"],
+      default: "learner",
+    },
+    bio: String,
+    subjects: [String],
+    hourlyRate: {
+      type: Number,
+      default: 100,
+    },
+    availability: [String],
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    reviewCount: {
+      type: Number,
+      default: 0,
+    },
+    totalSessions: {
+      type: Number,
+      default: 0,
+    },
+    profileImage: String,
+    enrolledTopics: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Topic",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+)
+
+userSchema.pre('save', async function (next){
+    if (!this.isModified('password')){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+export default mongoose.model('User')
