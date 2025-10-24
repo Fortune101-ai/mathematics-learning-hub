eaca5f201ffee999f3816944f480c8571249adea
Fortune Mahloane <sensefortune01@gmail.com>
2025-10-24 11:12:51 +0200

    add basic project setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors({origin: '*',credentials: true}))
+app.use(express.json())
+
+app.get('/api/health', (req, res) => {
+ res.json({ status: 'OK', message: 'Server is running' })
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
