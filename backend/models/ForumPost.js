86ef5c6b2a3e9ddb9f0fc15e7f7472f8d869afb2
Fortune Mahloane <sensefortune01@gmail.com>
2025-10-30 19:35:22 +0200

    Add database schema for a forum post

---
+++ backend/models/ForumPost.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const replySchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true,
+    },
+    authorId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    },
+    authorName: {
+        type:String,
+        default:'Anonymous'
+    },
+    verified:{
+        type:Boolean,
+        default:false,
+    },
+    votes: {
+        type:Number,
+        default:0,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    }
+},{
+    timestamps:true
+})
+
+const forumPostSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    content:{
+        type:String,
+        required:true,
+    },
+    authorId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    },
+    authorName:{
+        type:String,
+        default:'Anonymous'
+    },
+    topic:{
+        topic:String,
+        required:true,
+    },
+    replies:[replySchema],
+    views:{
+        type:Number,
+        default:0,
+    },
+    verified:{
+        type:Boolean,
+        default:false,
+    },
+    votes:{
+        type:Number,
+        default:0,
+    }
+},{
+    timestamps:true
+})
+
+const ForumPost = mongoose.model('ForumPost', forumPostSchema);
+
+
